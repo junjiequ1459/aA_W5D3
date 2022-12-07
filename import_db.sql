5384fa1be7294813a185708d6a42fe3bcc2f1121
rexqu <junjiequ123@gmail.com>
2022-12-07 11:27:39 -0500

    worked on sql

--- import_db.sql
+++ import_db.sql
@@ -1,1 +1,35 @@
--- testing testing
+PRAGMA foreign_keys = ON;
+
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL,
+)
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author TEXT NOT NULL
+)
+
+CREATE TABLE question_follows (
+    user_id INTEGER PRIMARY KEY,
+    author TEXT NOT NULL 
+)
+
+
+CREATE TABLE replies ( 
+    reply TEXT NOT NULL,
+    reply REFERENCES questions(id)
+    reply REFERENCES replies(reply)
+    reply REFERENCES user(id)
+    )
+
+CREATE TABLES question_likes (
+    likes INTEGER NOT NULL,
+    
+    likes REFERENCES users(id)
+    likes REFERENCES questions(id)
+)
