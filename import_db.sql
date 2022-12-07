3ba55cf9c211114422731bb6b1c43bf749d97a1c
Stiven Kang <kdaegyung@gmail.com>
2022-12-07 11:47:48 -0500

    completed tables

--- import_db.sql
+++ import_db.sql
@@ -4,32 +4,43 @@ PRAGMA foreign_keys = ON;
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
-    lname TEXT NOT NULL,
-)
+    lname TEXT NOT NULL
+);
 
 CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
     author TEXT NOT NULL
-)
+);
 
 CREATE TABLE question_follows (
     user_id INTEGER PRIMARY KEY,
     author TEXT NOT NULL 
-)
+);
 
 
 CREATE TABLE replies ( 
-    reply TEXT NOT NULL,
-    reply REFERENCES questions(id)
-    reply REFERENCES replies(reply)
-    reply REFERENCES user(id)
-    )
+    body TEXT NOT NULL,
+    FOREIGN KEY (body) REFERENCES questions(id),
+    FOREIGN KEY (body) REFERENCES replies(body),
+    FOREIGN KEY (body) REFERENCES user(id)
 
-CREATE TABLES question_likes (
+);
+
+CREATE TABLE question_likes (
     likes INTEGER NOT NULL,
     
-    likes REFERENCES users(id)
-    likes REFERENCES questions(id)
-)
+    FOREIGN KEY (likes) REFERENCES users(id),
+    FOREIGN KEY (likes) REFERENCES questions(id)
+);
+
+INSERT INTO
+    users (fname, lname)
+VALUES
+    ('first', 'last');
+
+INSERT INTO
+    questions (title, body, author)
+VALUES
+    ('title', 'body', 'author');
